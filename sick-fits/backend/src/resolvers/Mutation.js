9802073a430503d4d745277bfbe9880ee171a576
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-06-07 16:36:20 -0500

    Created Item datamodel, a mutation to create Item and a query to get all items. Did this through the whole flow from Yoga to Prisma. There are many files that needs to be modified for this. Everytime you change the model, you need to deploy it.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,8 +1,20 @@
 const Mutations = {
-	createDog(parent, args, ctx, info) {
+	async createItem(parent, args, ctx, info) {
+		// TODO Check if they are logged in
+		// Here we are going to interface with the Prisma database, the API is everything inside prisma.graphq
+		const item = await ctx.db.mutation.createItem({
+			data: {
+				// Spreading the args argument
+				...args
+			}
+		}, info);
+
+		return item;
+	}
+	/*createDog(parent, args, ctx, info) {
 		// create a dog
 		console.log(args);
-	}
+	}*/
 };
 
 module.exports = Mutations;
