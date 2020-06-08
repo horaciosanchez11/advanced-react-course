f8f089e97b93e3b17a0526a3513a017d5d02c51a
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-06-07 19:38:16 -0500

    Video 15 - Introduced Apollo within the front-end fo the application. Apollo will serve as data management storage, something that redux can do, kind of an alternative to that.

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,34 @@
 import App, {Container} from 'next/app';
 import Page from '../components/Page';
+import {ApolloProvider} from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+	// this method runs first, it comes from nextJS; it runs before the initial render happens
+	static async getInitialProps({Component, ctx}) {
+		let pageProps = {};
+		if (Component.getInitialProps) {
+			pageProps = await  Component.getInitialProps(ctx);
+		}
+		// this exposes the query to the user
+		pageProps.query = ctx.query;
+		// you expose via props with this
+		return {pageProps};
+	}
+
 	render() {
-		const {Component} = this. props;
+		const {Component, apollo, pageProps} = this. props;
 
 		return(
 			<Container>
-				<Page>
-					<Component />
-				</Page>
+				<ApolloProvider client={apollo}>
+					<Page>
+						<Component />
+					</Page>
+				</ApolloProvider>
 			</Container>
 		)
 	}
 }
 
-export default MyApp;
+export default withData(MyApp);
