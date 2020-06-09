3050ddbbb7caa4a1ea464005092dec22fe1525fa
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-06-09 14:58:04 -0500

    Video 20 - Created the first iteration of the D part of CRUD. We are now able to delete an item and refresh the page/cache. Had an error in the mutation resolver where I wrote dB instead of db and that was causing a hard to trace error

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,37 +1,47 @@
 const Mutations = {
-  async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+	async createItem(parent, args, ctx, info) {
+		// TODO Check if they are logged in
+		// Here we are going to interface with the Prisma database, the API is everything inside prisma.graphq
+		const item = await ctx.db.mutation.createItem({
+			data: {
+				// Spreading the args argument
+				...args
+			}
+		}, info);
 
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          ...args,
-        },
-      },
-      info
-    );
+		return item;
+	},
+	
+	updateItem(parent, args, ctx, info) {
+		// Take a copy of the updates
+		const updates = { ...args};
+		// remove the ID from the updates
+		delete updates.id;
+		// run the update method
+		return ctx.db.mutation.updateItem({
+			data: updates,
+			where: {
+				id: args.id
+			}
+		}, info
+		);
+	},
 
-    console.log(item);
+	async deleteItem(parent, args, ctx, info) {
+		const where = {id: args.id};
+		// first find the item
+		const item = await ctx.db.query.item({where}, `{
+			id
+			title
+		}`);
 
-    return item;
-  },
-  
-  updateItem(parent, args, ctx, info) {
-    // first take a copy of the updates
-    const updates = { ...args };
-    // remove the ID from the updates
-    delete updates.id;
-    // run the update method
-    return ctx.db.mutation.updateItem(
-      {
-        data: updates,
-        where: {
-          id: args.id,
-        },
-      },
-      info
-    );
-  },
+		// check if they own the item or have the permissions
+
+
+		// delete item
+		return ctx.db.mutation.deleteItem({where}, info);
+	}
+
 };
 
 module.exports = Mutations;
