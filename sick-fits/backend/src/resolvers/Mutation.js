c615036e2f4b807bd74bd7e55aa064433acacf8a
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-06-09 13:08:30 -0500

    Worked on the U of CRUD. Created UpdateItem component and page. Added a single item query in the backend. There was a missing bit of info in _app.js that was causing an error; a props wasn't been sent to inner pages

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,20 +1,37 @@
 const Mutations = {
-	async createItem(parent, args, ctx, info) {
-		// TODO Check if they are logged in
-		// Here we are going to interface with the Prisma database, the API is everything inside prisma.graphq
-		const item = await ctx.db.mutation.createItem({
-			data: {
-				// Spreading the args argument
-				...args
-			}
-		}, info);
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
 
-		return item;
-	}
-	/*createDog(parent, args, ctx, info) {
-		// create a dog
-		console.log(args);
-	}*/
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  },
+  
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,34 +1,31 @@
-import App, {Container} from 'next/app';
+import App, { Container } from 'next/app';
 import Page from '../components/Page';
-import {ApolloProvider} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
 
 class MyApp extends App {
-	// this method runs first, it comes from nextJS; it runs before the initial render happens
-	static async getInitialProps({Component, ctx}) {
-		let pageProps = {};
-		if (Component.getInitialProps) {
-			pageProps = await  Component.getInitialProps(ctx);
-		}
-		// this exposes the query to the user
-		pageProps.query = ctx.query;
-		// you expose via props with this
-		return {pageProps};
-	}
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+  render() {
+    const { Component, apollo, pageProps } = this.props;
 
-	render() {
-		const {Component, apollo, pageProps} = this. props;
-
-		return(
-			<Container>
-				<ApolloProvider client={apollo}>
-					<Page>
-						<Component />
-					</Page>
-				</ApolloProvider>
-			</Container>
-		)
-	}
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
+    );
+  }
 }
 
 export default withData(MyApp);
