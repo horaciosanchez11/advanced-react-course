8035391c64888f8c94b28975a1eecf9d40f31e96
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-06-29 18:00:10 -0500

    Video 25 and 26. update user data model so that it takes a unique email, password, permission, token and token expiry time. There is a difference in the videos here, I had to add an addition @scalarList annotation to Permission. Started making the signup form in the front end.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,15 @@
 // let's go!
 // this is the entry point of the application
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-//TODO: Use Express middleware to handles cookies (JWT)
+//TODO: Use Express middleware to handles cookies (JWT) - DONE!!
+server.express.use(cookieParser());
+
 //TODO: Use Express middleware to populate current user
 
 server.start({
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		// TODO Check if they are logged in
@@ -40,7 +44,37 @@ const Mutations = {
 
 		// delete item
 		return ctx.db.mutation.deleteItem({where}, info);
-	}
+	},
+
+	async signup(parent, args, ctx, info) {
+		args.email = args.email.toLowerCase();
+
+		// hash password
+		const password = await bcrypt.hash(args.password, 10);
+
+		// create user in DB
+		const user = await ctx.db.mutation.createUser({
+			data: {
+				...args,
+				password: password,
+				permissions: {set: ['USER']}
+			}
+		}, info);
+
+		// create JWT token
+		const token = jwt.sign({
+			userId: user.id
+		}, process.env.APP_SECRET);
+
+		// set jwt as cookie
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+
+		// return the user to browser
+		return user;
+	} 
 
 };
 
