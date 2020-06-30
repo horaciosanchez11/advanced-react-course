460a7e4c50c717ba84201c4d2dadf97486f87ce4
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-06-30 17:51:01 -0500

    Video 26. We are retrieving the logged in user, or no user, for every request done to the server. We are getting the jwt token, get the user Id, and displaying it on top of the page

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,6 +1,7 @@
 // let's go!
 // this is the entry point of the application
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -9,6 +10,19 @@ const server = createServer();
 
 //TODO: Use Express middleware to handles cookies (JWT) - DONE!!
 server.express.use(cookieParser());
+
+// decode JWT to get user Id on each request
+server.express.use((req, res, next) => {
+	const {token} = req.cookies;
+	if (token) {
+		const {userId} = jwt.verify(token, process.env.APP_SECRET);
+
+		// put user Id onto request
+		req.userId = userId;
+	}
+
+	next();
+});
 
 //TODO: Use Express middleware to populate current user
 
