23b6003ba35d221fa7299742cd46a8300c2a94c8
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-07-02 19:16:21 -0500

    Started working on a permissions management page. So far we are querying and showing all users and possible permissions on a page

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -24,6 +24,21 @@ server.express.use((req, res, next) => {
 	next();
 });
 
+// create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+	// if they are not logged in, skip this
+	if (!req.userId) return next();
+
+	const user = await db.query.user({
+		where: {
+				id: req.userId
+			}
+		}, '{id, permissions, email, name}'
+	);
+	req.user = user;
+	next();
+});
+
 //TODO: Use Express middleware to populate current user
 
 server.start({
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const {hasPermission} = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -12,6 +13,19 @@ const Query = {
   	return ctx.db.query.user({
   		where: {id: ctx.request.userId}
   	}, info);
+  },
+
+  async users(parent, args, ctx, info) {
+    // check if they are login
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in');
+    }
+
+    // check if user has the permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN','PERMISSIONUPDATE']);
+
+    // if they have permissions, query all users
+    return ctx.db.query.users({}, info);
   }
   // async items(parent, args, ctx, info) {
   //   console.log('Getting Items!!');
