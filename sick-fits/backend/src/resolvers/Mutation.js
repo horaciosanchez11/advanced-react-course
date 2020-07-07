5862fd28f5aeafbaf37d095ae660f94c67b5bf74
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-07-07 13:21:37 -0500

    Video 40. Started working on the back end part of adding items to the cart. Created CartItem data model.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -232,6 +232,43 @@ const Mutations = {
 				id: args.userId
 			}
 		}, info);
+	},
+
+	async addToCart(parent, args, ctx, info) {
+		// signed in?
+		const {userId} = ctx.request;
+		if (!userId) {
+			throw new Error('You must be signed in');
+		}
+
+		// query current cart
+		const [existingCartItem] = await ctx.db.query.cartItems({
+			where: {
+				user: {id: userId},
+				item: {id: args.id}
+			}			
+		});
+
+		// check if item is already in cart
+		if (existingCartItem) {
+			// item already in cart
+			return ctx.db.mutation.updateCartItem({
+				where: {id: existingCartItem.id},
+				data: {quantity: existingCartItem.quantity + 1}
+			}, info);
+		}
+
+		// if not, create a new cart item for that user
+		return ctx.db.mutation.createCartItem({
+			data: {
+				user: {
+					connect: {id: userId}
+				},
+				item: {
+					connect: {id: args.id}
+				}
+			}
+		}, info);
 	}
 
 };
