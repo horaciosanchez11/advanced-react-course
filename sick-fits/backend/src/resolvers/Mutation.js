852911c8c0be3adec64d1d683c79c4d0422d5dda
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-07-06 19:15:38 -0500

    Video 36. Finished connecting backend with front end for the update permissions page

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const {randomBytes} = require('crypto');
 const{promisify} = require('util');
 const {transport, makeANiceEmail} = require('../mail');
+const {hasPermission} = require('../utils');
 
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
@@ -194,6 +195,35 @@ const Mutations = {
 
 		// return the new user
 		return updatedUser;
+	},
+
+	async updatePermissions(parent, args, ctx, info) {
+		// check if login
+		if (!ctx.request.userId) {
+			throw new Error('You must be logged in');
+		}
+
+		// query current user
+		const currentUser = await ctx.db.query.user({
+			where: {
+				id: ctx.request.userId
+			}
+		}, info);
+
+		// check if they have permission
+		hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+
+		// update permission
+		return ctx.db.mutation.updateUser({
+			data: {
+				permissions: {
+					set: args.permissions
+				}
+			},
+			where: {
+				id: args.userId
+			}
+		}, info);
 	}
 
 };
