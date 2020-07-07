15137df3d86dd8c6682a59843144b054cf61a404
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-07-07 12:36:29 -0500

    Video 39. Worked on the close button to hide the Cart snippet. Also added a button to show it. We are using local state with apollo cliente ti retrieve/set the state of the snipper; opened or closed

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,24 +1,46 @@
 import React from 'react';
+import {Query, Mutation} from 'react-apollo';
+import gql from 'graphql-tag';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
 
+const LOCAL_STATE_QUERY = gql`
+	query {
+		cartOpen @client
+	}
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+	mutation {
+		toggleCart @client
+	}
+`;
+
 const Cart = () => {
 	return (
-		<CartStyles open>
-			<header>
-				<CloseButton title="close">&times;</CloseButton>
-				<Supreme>Your Cart</Supreme>
-				<p>You have x items in your cart.</p>
-				<footer>
-					<p>$0.00</p>
-					<SickButton>Checkout</SickButton>
-				</footer>
-			</header>
-		</CartStyles>		
+		<Mutation mutation={TOGGLE_CART_MUTATION}>
+			{toggleCart => (
+				<Query query={LOCAL_STATE_QUERY}>		
+					{({data}) => (
+						<CartStyles open={data.cartOpen}>
+							<header>
+								<CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
+								<Supreme>Your Cart</Supreme>
+								<p>You have x items in your cart.</p>
+								<footer>
+									<p>$0.00</p>
+									<SickButton>Checkout</SickButton>
+								</footer>
+							</header>
+						</CartStyles>
+					)}
+				</Query>
+			)}
+		</Mutation>
 	)
 };
 
 export default Cart;
-
+export {LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION};
