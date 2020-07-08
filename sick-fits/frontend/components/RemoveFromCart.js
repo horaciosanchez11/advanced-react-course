b6c3d012e778f9545473efcea7443f538df113e5
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-07-08 15:55:58 -0500

    Video 43 and 44. Added functionality to remove items visually when removed from cart. Also, added a bubble at the right of the Cart button that displays the number of items in the cart and gets automatically updated when adding a new item

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -28,9 +28,38 @@ class RemoveFromCart extends React.Component {
 		id: PropTypes.string.isRequired
 	};
 
+	// gets called as soon as we get a response back from server after a mutation is performed
+	update = (cache, payload) => {
+		// read the cache
+		const data = cache.readQuery({
+			query: CURRENT_USER_QUERY
+		});
+
+		// remove that item from cart
+		const cartItemId = payload.data.removeFromCart.id;
+		data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+
+		// write it back to the cache
+		cache.writeQuery({
+			query: CURRENT_USER_QUERY,
+			data: data
+		});
+
+	}
+
 	render() {
 		return (
-			<Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{id:this.props.id}}>
+			<Mutation 
+			 mutation={REMOVE_FROM_CART_MUTATION} 
+			 variables={{id:this.props.id}} 
+			 update={this.update} 
+			 optimisticResponse={{
+			 	__typename: 'Mutation',
+			 	removeFromCart: {
+			 		__typename: 'CartItem',
+			 		id: this.props.id
+			 	}
+			 }}>
 				{(removeFromCart, {loading, error}) => (
 					<BigButton
 						disabled={loading}
