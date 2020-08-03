ae39ac86b3e14f9fdce1e19453e678dabe45d8a2
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-08-02 21:30:01 -0500

    Video 61. Tested Pagination component

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -24,7 +24,7 @@ const Pagination = props => (
 		const pages = Math.ceil(count / perPage);
 		const page = props.page;
 		return (
-			<PaginationStyles>
+			<PaginationStyles data-test="pagination">
 				<Head>
 					<title>Sick Fits | Page {props.page} of {pages}</title>
 				</Head>
@@ -34,13 +34,13 @@ const Pagination = props => (
 				}}>
 					<a className="prev" aria-disabled={page <= 1}>Prev</a>
 				</Link>
-				<p>Page {props.page} of {pages}</p>
+				<p>Page {props.page} of <span className="totalPages">{pages}</span></p>
 				<p>{count} Items Total</p>
 				<Link prefetch href={{
 					pathname: 'items',
 					query: {page: page + 1}
 				}}>
-					<a className="prev" aria-disabled={page >= pages}>Next</a>
+					<a className="next" aria-disabled={page >= pages}>Next</a>
 				</Link>
 			</PaginationStyles>
 	)}}
@@ -49,3 +49,4 @@ const Pagination = props => (
 )
 
 export default Pagination;
+export {PAGINATION_QUERY};
